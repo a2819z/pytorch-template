c0e9e917ebd7250546cb46911907f840e986c84e
JeongsikLee <a2819z2@gmail.com>
2021-04-05 15:32:18 +0900

    [feat] basic code for train (except training loop)

--- train.py
+++ train.py
@@ -1,17 +1,21 @@
 import argparse
 import os
 from pathlib import Path
+import time
 
 import yaml
 
 import torch
+import torch.nn as nn
+import torch.optim as optim
 import torch.distributed as dist
+from torch.cuda import amp
 from torch.nn.parallel import DistributedDataParallel as DDP
 from torch.utils.tensorboard import SummaryWriter
 
-from models.model import Model
+from tqdm import tqdm
 
-from utils.torch_utils import select_device
+from utils.torch_utils import ModelEMA, select_device, intersect_dicts, freeze_layer
 from utils.general import init_seed
 from utils.datasets import create_dataloader
 
@@ -40,31 +44,104 @@ def train(hyp, opt: argparse.Namespace, device: torch.device, tb_writer: Summary
         data_dict = yaml.load(f, Loader=yaml.SafeLoader)
 
     # Model
-    pretrained = weights.endswith(".pth")
-    if pretrained:
-        pass
-    else:
-        model = Model(opt.cfg).to(device)
+    model = init_model(opt, weights, device)
+    freeze_layer(model, opt.hyp.freeze)
 
     train_path = data_dict["train"]
     val_path = data_dict["val"]
 
     optimizer = init_optimzer(opt, model)
 
+    ema = ModelEMA(model) if rank in [-1, 0] else None
+
+    start_epoch = resume(weights, optimizer, ema, device)
+
     # DP mode
     if cuda and rank == -1 and torch.cuda.device_count() > 1:
         model = torch.nn.DataParallel(model)
 
-    dataloader, dataset = create_dataloader()
+    dataloader, dataset = create_dataloader(
+        train_path,
+        batch_size,
+        opt,
+        hyp=hyp,
+        rank=rank,
+        world_size=opt.world_size,
+        workers=opt.workers,
+    )
+    nb = len(dataloader)
+    # Process 0
     if rank in [-1, 0]:
-        testloader, testset = create_dataloader()
+        testloader, testset = create_dataloader(
+            val_path,
+            batch_size * 2,
+            opt,
+            hyp=hyp,
+            rank=-1,
+            world_size=opt.world_size,
+            workers=opt.workers,
+        )
 
     if cuda and rank != -1:
         model = DDP(model, device_ids=[opt.local_rank], output_device=opt.local_rank)
 
+    scaler = amp.GradScaler(enabled=opt.hyp.amp)
+    # TODO: LossComputer
 
-def init_optimzer(opt, model):
-    pass
+    # Start training
+    t0 = time.time()
+    for epoch in range(start_epoch, epochs):
+        model.train()
+        if rank != -1:
+            dataloader.sampler.set_epoch(epoch)
+        pbar = enumerate(dataloader)
+        if rank in [-1, 0]:
+            pbar = tqdm(pbar, total=nb)
+
+        optimizer.zero_grad()
+        for i, (img, labels) in pbar:
+            # TODO: Training process
+            pass
+
+
+def init_model(opt, weights, device):
+    module = __import__(f"models.{opt.cfg.module}")
+    # TODO: Model configure opt
+    model = getattr(module, f"{opt.cfg.architecture}").to(device)
+
+    pretrained = weights.endswith(".pth")
+    if pretrained:
+        checkpoint = torch.load(weights, map_location=device)
+        state_dict = checkpoint["model"]
+        state_dict = intersect_dicts(state_dict, model.state_dict())
+        model.load_state_dict(state_dict, strict=False)
+
+    return model
+
+
+def init_optimzer(opt, model: nn.Module) -> optim.Optimizer:
+    eps = 1e-4 if opt.hyp.amp else 1e-8
+    args = opt.hyp.optim_args
+    kwargs = {"lr": opt.hyp.lr, "params": model.parameters(), "eps": eps}
+
+    return getattr(optim, opt.hyp.optimizer)(*args, **kwargs)
+
+
+def resume(weights, optimizer: optim.Optimizer, ema: ModelEMA, device, results):
+    start_epoch = 0
+    pretrained = weights.endswith(".pth")
+    if pretrained:
+        checkpoint = torch.load(weights, map_location=device)
+        if (ckpt_optim := checkpoint.get("optimizer", None)) is not None:
+            optimizer.load_state_dict(ckpt_optim)
+
+        if ema and (ckpt_ema := checkpoint.get("ema", False)):
+            ema.ema.load_state_dict(ckpt_ema.state_dict())
+            ema.updates = ckpt_ema["updates"]
+
+        start_epoch = checkpoint["epoch"] + 1
+
+    return start_epoch
 
 
 if __name__ == "__main__":
@@ -81,7 +158,7 @@ if __name__ == "__main__":
     parser.add_argument(
         "--device", default="", help="cuda devices, i.e. 0 or 1,2,3,4 or cpu"
     )
-
+    parser.add_argument("--workers", type=int, default=4)
     parser.add_argument("--local_rank", default=-1, help="never change this value")
     opt = parser.parse_args()
 
